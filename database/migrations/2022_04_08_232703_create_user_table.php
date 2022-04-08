06b1082879b10f22ddee5d8f6be6e14d91e5744a
lequoctoan1234 <noname2k2123@gmail.com>
2022-04-08 23:36:28 +0700

    Migration

---
+++ database/migrations/2022_04_08_232703_create_user_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateUserTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('user', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('email')->unique();
+            $table->string('passwork');
+            $table->string('address')->nullable();
+            $table->string('phone')->unique();
+
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('user');
+    }
+}
